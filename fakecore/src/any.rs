5322a935011899272857e790246b838f3703faec
Jane Lusby <jlusby@yaah.dev>
2021-04-12 14:32:23 -0700

    move submodules into inline modules

--- fakecore/src/any.rs
+++ fakecore/src/any.rs
@@ -6,8 +6,131 @@ extern crate alloc;
 #[cfg(feature = "alloc")]
 use alloc::boxed::Box;
 
-pub mod provider;
-pub mod tag;
+pub mod provider {
+    //! Tag-based value lookup API for trait objects.
+    //!
+    //! This provides a similar API to my `object_provider` crate, built on top of
+    //! `dyno`
+
+    use super::{Tag, Tagged};
+
+    /// An untyped request for a value of a specific type.
+    ///
+    /// This type is generally used as an `&mut Request<'a>` outparameter.
+    #[repr(transparent)]
+    pub struct Request<'a> {
+        tagged: dyn Tagged<'a> + 'a,
+    }
+
+    impl<'a> Request<'a> {
+        /// Helper for performing transmutes as `Request<'a>` has the same layout as
+        /// `dyn Tagged<'a> + 'a`, just with a different type!
+        ///
+        /// This is just to have our own methods on it, and less of the interface
+        /// exposed on the `provide` implementation.
+        fn wrap_tagged<'b>(t: &'b mut (dyn Tagged<'a> + 'a)) -> &'b mut Self {
+            unsafe { &mut *(t as *mut (dyn Tagged<'a> + 'a) as *mut Request<'a>) }
+        }
+
+        pub fn is<I>(&self) -> bool
+        where
+            I: Tag<'a>,
+        {
+            self.tagged.is::<ReqTag<I>>()
+        }
+
+        pub fn provide<I>(&mut self, value: I::Type) -> &mut Self
+        where
+            I: Tag<'a>,
+        {
+            if let Some(res @ None) = self.tagged.downcast_mut::<ReqTag<I>>() {
+                *res = Some(value);
+            }
+            self
+        }
+
+        pub fn provide_ref<I: ?Sized + 'static>(&mut self, value: &'a I) -> &mut Self
+        {
+            use crate::any::tag::Ref;
+            if let Some(res @ None) = self.tagged.downcast_mut::<ReqTag<Ref<I>>>() {
+                *res = Some(value);
+            }
+            self
+        }
+
+        pub fn provide_with<I, F>(&mut self, f: F) -> &mut Self
+        where
+            I: Tag<'a>,
+            F: FnOnce() -> I::Type,
+        {
+            if let Some(res @ None) = self.tagged.downcast_mut::<ReqTag<I>>() {
+                *res = Some(f());
+            }
+            self
+        }
+    }
+
+    pub trait Provider {
+        fn provide<'a>(&'a self, request: &mut Request<'a>);
+    }
+
+    impl dyn Provider {
+        pub fn request<'a, I>(&'a self) -> Option<I::Type>
+        where
+            I: Tag<'a>,
+        {
+            request::<I, _>(|request| self.provide(request))
+        }
+    }
+
+    pub fn request<'a, I, F>(f: F) -> Option<<I as Tag<'a>>::Type>
+    where
+        I: Tag<'a>,
+        F: FnOnce(&mut Request<'a>),
+    {
+        let mut result: Option<<I as Tag<'a>>::Type> = None;
+        f(Request::<'a>::wrap_tagged(<dyn Tagged>::tag_mut::<ReqTag<I>>(
+            &mut result,
+        )));
+        result
+    }
+
+    /// Implementation detail: Specific `Tag` tag used by the `Request` code under
+    /// the hood.
+    ///
+    /// Composition of `Tag` types!
+    struct ReqTag<I>(I);
+    impl<'a, I: Tag<'a>> Tag<'a> for ReqTag<I> {
+        type Type = Option<I::Type>;
+    }
+}
+
+pub mod tag {
+    //! Simple type-based tag values for use in generic code.
+    use super::Tag;
+    use core::marker::PhantomData;
+
+    /// Type-based `Tag` for `&'a T` types.
+    pub struct Ref<T: ?Sized + 'static>(PhantomData<T>);
+
+    impl<'a, T: ?Sized + 'static> Tag<'a> for Ref<T> {
+        type Type = &'a T;
+    }
+
+    /// Type-based `Tag` for `&'a mut T` types.
+    pub struct RefMut<T: ?Sized + 'static>(PhantomData<T>);
+
+    impl<'a, T: ?Sized + 'static> Tag<'a> for RefMut<T> {
+        type Type = &'a mut T;
+    }
+
+    /// Type-based `Tag` for concrete types.
+    pub struct Value<T: 'static>(PhantomData<T>);
+
+    impl<T: 'static> Tag<'_> for Value<T> {
+        type Type = T;
+    }
+}
 
 /// An identifier which may be used to tag a specific
 pub trait Tag<'a>: Sized + 'static {
